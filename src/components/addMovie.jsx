22e537c0979098e7074757d83bfae5f3db79b421
Grigoriy L <grisha2198@gmail.com>
2022-09-30 21:52:58 +0300

    first commit

---
+++ src/components/addMovie.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const AddMovie = (props) => {
+  return <button className="btn btn-primary new-movie">New Movie</button>;
+};
+
+export default AddMovie;
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import * as userService from "../services/userService";
+import auth from "../services/authService";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username").email(),
+    password: Joi.string().required().label("Password").min(5).max(30),
+    name: Joi.string().required().label("Name").min(2).max(30),
+  };
+
+  doSubmit = async () => {
+    try {
+      const response = await userService.register(this.state.data);
+      auth.loginWithJwt(response.headers["x-auth-token"]);
+      window.location = "/";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", "email")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
